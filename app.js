cec6d333a5a3329832bf3846cb571eb4e19f957b
Shobhit kumar parashar <shobhitparashar0@gmail.com>
2023-11-05 18:36:48 +0530

    mongo

--- app.js
+++ app.js
@@ -24,29 +24,29 @@ const userRoutes = require("./routes/user.js");  // this is for user route
 const port = process.env.PORT || 3001;
 const dbpass = process.env.DB_PASS || "";
 
-// data_Base Conection
-main().then((res) => {
-    console.log("connected to DB");
-})
-    .catch(err => console.log(err));
-
-async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/airBnb');
-}
-
-
+// // data_Base Conection
+// main().then((res) => {
+//     console.log("connected to DB");
+// })
+//     .catch(err => console.log(err));
 
 // async function main() {
-//     const uri = "mongodb+srv://shobhit:" + dbpass + "@cluster0.snn3wbn.mongodb.net/airBnb?retryWrites=true&w=majority";
-//     await mongoose.connect(uri, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//     });
-
-//     console.log('Connected to MongoDB Atlas');
+//     await mongoose.connect('mongodb://127.0.0.1:27017/airBnb');
 // }
 
-// main().catch((err) => console.log(err));
+
+
+async function main() {
+    const uri = "mongodb+srv://shobhit:" + dbpass + "@cluster0.snn3wbn.mongodb.net/airBnb?retryWrites=true&w=majority";
+    await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    console.log('Connected to MongoDB Atlas');
+}
+
+main().catch((err) => console.log(err));
 
 app.use(cookiesParser());
 app.set("view engine", "ejs");
